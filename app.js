061d8427acf7b6551d892526a810396ec610e035
sathvikamuktha <sathvikamuktha@gmail.com>
2025-03-10 23:27:43 -0400

    update app.js

--- app.js
+++ app.js
@@ -3,28 +3,23 @@ const app = express();
 const methodOverride = require('method-override');
 const items = require('./models/items');
 const itemsRoutes = require('./routes/itemsRoutes'); 
-
 // configure app
 let port = 3000;
 let host = 'localhost';
 app.set('view engine', 'ejs');
-
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method')); 
 app.use('/items', itemsRoutes);
-
 // Render home page
 app.get('/', (req, res) => {
     res.render('index');
 });
-
 // Render login page
 app.get('/login', (req, res) => {
     res.render('login');
 });
-
 // Render signup page
 app.get('/signup', (req, res) => {
     res.render('signup');
